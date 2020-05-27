8e54fb564ee36cbb380fc71d866fb56ebce10b2c
goosebumps98 <61392202+goosebumps98@users.noreply.github.com>
2020-05-26 21:50:34 -0400

    Update Loader.php

--- src/Hytlenz/Loader.php
+++ src/Hytlenz/Loader.php
@@ -3,33 +3,33 @@
 namespace Hytlenz;
 
 //Consumables
-use Hytlenz\Consume\Alcohol;
+//use Hytlenz\Consume\Alcohol;
 use Hytlenz\Consume\Weeds;
-use Hytlenz\Consume\Cocaine;
-use Hytlenz\Consume\Cigarette;
-use Hytlenz\Consume\Vape;
+//use Hytlenz\Consume\Cocaine;
+//use Hytlenz\Consume\Cigarette;
+//use Hytlenz\Consume\Vape;
 //Weapons
-use Hytlenz\Weapons\HandGrenade;
-use Hytlenz\Weapons\CrackShot;
+//use Hytlenz\Weapons\HandGrenade;
+//use Hytlenz\Weapons\CrackShot;
 
-use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
-use pocketmine\utils\Config;
+//use pocketmine\event\Listener;
+//use pocketmine\plugin\PluginBase;
+//use pocketmine\Server;
+//use pocketmine\utils\Config;
 
 class Loader extends PluginBase implements Listener {
 	
 	public function onEnable() {
 		//Consumables (Dont say it in PDEA that we have good shits XD)
-		$this->getServer()->getPluginManager()->registerEvents(new Alcohol($this), $this);
+		//$this->getServer()->getPluginManager()->registerEvents(new Alcohol($this), $this);
 		$this->getServer()->getPluginManager()->registerEvents(new Weeds($this), $this);
-		$this->getServer()->getPluginManager()->registerEvents(new Cocaine($this), $this);
-		$this->getServer()->getPluginManager()->registerEvents(new Cigarette($this), $this);
-		$this->getServer()->getPluginManager()->registerEvents(new Vape($this), $this);
+		//$this->getServer()->getPluginManager()->registerEvents(new Cocaine($this), $this);
+		//$this->getServer()->getPluginManager()->registerEvents(new Cigarette($this), $this);
+		//$this->getServer()->getPluginManager()->registerEvents(new Vape($this), $this);
 		//Weapons (Use it Properly)
-		$this->getServer()->getPluginManager()->registerEvents(new HandGrenade($this), $this);
-		$this->getServer()->getPluginManager()->registerEvents(new CrackShot($this), $this);
-		$this->getServer()->getLogger()->notice("PocketGuns was Enabled!");
+		//$this->getServer()->getPluginManager()->registerEvents(new HandGrenade($this), $this);
+		//$this->getServer()->getPluginManager()->registerEvents(new CrackShot($this), $this);
+		//$this->getServer()->getLogger()->notice("PocketGuns was Enabled!");
 		
 	}
 }	
